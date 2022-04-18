82bd035c5ed1304cc74e238d706abdcb202756ea
Glayton Martins <gr.martins@outlook.com>
2022-04-18 20:52:04 -0300

    link click tracking

---
+++ bookmarks/redirect_to_url.py
@@ -0,0 +1,16 @@
+from bookmarks.database.models import Bookmark
+from bookmarks.database import db
+from flask import redirect
+
+
+def init_app(app):
+
+    @app.get('/<short_url>')
+    def redirect_to_url(short_url):
+        bookmark = Bookmark.query.filter_by(short_url=short_url).first_or_404()
+
+        if bookmark:
+            bookmark.visits = bookmark.visits + 1
+            db.session.commit()
+
+            return redirect(bookmark.url)
