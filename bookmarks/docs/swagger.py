9c9edce318ea48dab14b708115d445ba91d530c3
Glayton Martins <gr.martins@outlook.com>
2022-04-21 12:16:33 -0300

    swagger OpenAPI host changeded

--- bookmarks/docs/swagger.py
+++ bookmarks/docs/swagger.py
@@ -12,6 +12,9 @@ template = {
         "termsOfService": "www.twitter.com/deve",
         "version": "1.0"
     },
+    "host": [
+        "https://booksmark.herokuapp.com"
+    ]
     "basePath": "/",  # base bash for blueprint registration
     "schemes": [
         "http",
