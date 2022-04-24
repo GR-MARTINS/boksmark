5e86a5943791c3ea1689876f028890cda2b7d672
Glayton Martins <gr.martins@outlook.com>
2022-04-24 12:55:29 -0300

    register site-bluprint

--- bookmarks/controllers/__init__.py
+++ bookmarks/controllers/__init__.py
@@ -1,7 +1,9 @@
 from bookmarks.controllers.api.auth import auth
 from bookmarks.controllers.api.bookmarks import bookmarks
+from bookmarks.controllers.frontend.site import site
 
 
 def init_app(app):
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
+    app.register_blueprint(site)
