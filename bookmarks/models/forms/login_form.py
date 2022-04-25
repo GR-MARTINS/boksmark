d75013d0125b1cd7515987f16c5cea295cee8a4f
Glayton Martins <gr.martins@outlook.com>
2022-04-25 19:43:19 -0300

    updated login form

--- bookmarks/models/forms/login_form.py
+++ bookmarks/models/forms/login_form.py
@@ -1,11 +1,12 @@
 import wtforms as wtf
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField
+
 
 class Login(FlaskForm):
     email = wtf.EmailField(
-        "E-mail", [wtf.validators.DataRequired(), wtf.validators.Email()]
+        "Email", [wtf.validators.DataRequired(), wtf.validators.Email()]
     )
     password = wtf.PasswordField(
-        "Senha", [wtf.validators.DataRequired()]
+        "Password", [wtf.validators.DataRequired()]
     )
+    remember = wtf.BooleanField("Remember-me")
